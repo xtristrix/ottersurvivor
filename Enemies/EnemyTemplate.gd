416c9bfbc6d21385a737e49226363c6a4b5d3e3c
Tristan Eberhart <xtristrix@cyberiade.de>
2022-11-16 19:53:09 +0100

    add health and die for enemies

--- Enemies/EnemyTemplate.gd
+++ Enemies/EnemyTemplate.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 onready var player = get_parent().get_node("Player")
 var path_to_player = null
 export var speed = 100
+export var health = 100
 
 
 func _ready():
@@ -11,9 +12,22 @@ func _ready():
 
 
 func _process(delta):
+	check_if_alive()
 	follow_player()
 	move_and_slide(path_to_player * speed)
 
 
+func hurt(damage):
+	health -= damage
+
+
 func follow_player():
 	path_to_player = (player.global_position - global_position).normalized()
+
+
+func check_if_alive():
+	if health <= 0:
+		die()
+
+func die():
+	queue_free()
