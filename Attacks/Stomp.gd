39a513b525f1d8af6868d74da1060d882967cbda
Tristan Eberhart <xtristrix@cyberiade.de>
2022-11-16 19:54:29 +0100

    add interactivity with other nodes for attacks

--- Attacks/Stomp.gd
+++ Attacks/Stomp.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
 
+export var damage = 100
+
+
+func _process(delta):
+	pass
 
 
 func _on_Timer_timeout():
 	queue_free()
+
+
+func _on_Stomp_body_entered(body):
+	if body.is_in_group("Enemies"):
+		body.hurt(damage)
